58e9280301e1eb75e6d4d85ec0b2360aa31e77fe
Никита Сыцевич <Nikita_Sytsevich@MacBook-Air-Maria.local>
2026-01-19 09:43:30 +0300

    BOOOb

--- src/features/articles/content/index.ts
+++ src/features/articles/content/index.ts
@@ -1,27 +1,23 @@
+// src/features/articles/content/index.ts
+
 import { howToPrepare } from './how-to-prepare';
 import { fluidBiohacking } from './fluid-biohacking';
 import { howToExit } from './how-to-exit';
-// Новые
-import { bombardPsychology } from './bombard-psychology';
-import { chinaStudy } from './china-study';
-import { mucuslessDiet } from './mucusless-diet';
-import { waterTruth } from './water-truth';
+import { livingFood } from './living-food';
 
 export const articles = [
-  // 1. Основы (Практика)
+  // 1. С чего начать (Вход)
   howToPrepare,
+  
+  // 2. Как не сорваться (Процесс)
   fluidBiohacking,
+  
+  // 3. Самое важное (Выход)
   howToExit,
   
-  // 2. Психология и Мотивация
-  bombardPsychology,
-  
-  // 3. Питание и Философия
-  chinaStudy,
-  mucuslessDiet,
-  
-  // 4. Альтернативный взгляд
-  waterTruth,
+  // 4. Глобальная цель (Философия и Питание)
+  livingFood,
 ];
 
+// Хелпер для поиска статьи по ID (используется в роутинге)
 export const getArticleById = (id: string) => articles.find(article => article.id === id);
